f7e73c4f499277721c003cd85f34426a91c1ec88
Your Name <you@example.com>
2017-11-02 23:18:11 -0200

    Connection Data Base

---
+++ Back.DB.CaseGerenciamentoTeste/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Back.DB.CaseGerenciamentoTeste.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Back.DB.CaseGerenciamentoTeste.Controllers
+{
+    public class AuthController : ApiController
+    {
+        [HttpPost]
+       // [AllowAnonymous]
+        [Route("api/auth")]
+        public HttpResponseMessage Auth(Auth auth)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            try
+            {
+                Back.DB.CaseGerenciamentoTeste.DataBase.Connection commm = new DataBase.Connection();
+                //  SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[statustype] ([statustype]) VALUES ('@teste')");
+                // cmd.Parameters.Add("@teste", SqlDbType.VarChar).Value = "oiii";
+                //cmd.CommandText = "INSERT INTO [dbo].[statustype] ([statustype]) VALUES ('teste') ";
+                // commm.execComand(cmd);
+                // commm.commit();
+
+                SqlCommand cmd = new SqlCommand("select * from [statustype] for json auto");
+                
+                response.StatusCode = HttpStatusCode.OK;
+                response.Content = new StringContent(JsonConvert.SerializeObject(auth));
+            }
+            catch(Exception ex)
+            {
+                response.StatusCode = HttpStatusCode.InternalServerError;
+                response.Content = new StringContent(ex.Message.ToString());
+            }
+            return response;
+            
+        }
+    }
+}
---
+++ Back.DB.CaseGerenciamentoTeste/Models/Auth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Back.DB.CaseGerenciamentoTeste.Models
+{
+    public class Auth
+    {
+        public string user { get; set; }
+        public string password { get; set; }
+        public string keyAcesso { get; set; }
+    }
+}
